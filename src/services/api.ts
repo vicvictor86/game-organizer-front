8f7fc16190185dab486a24ef32e3a93fb4b9c9fa
Victor Emmanuel <vicbem@hotmail.com>
2023-05-22 14:02:26 -0300

    Testing aws deploy connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backEndUrl = process.env.NODE_ENV === 'production' ? 'https://game-organizer.up.railway.app/' : 'http://localhost:3333/';
+const backEndUrl = process.env.NODE_ENV === 'production' ? 'http://54.235.46.90:3333/' : 'http://localhost:3333/';
 
 export const api = axios.create({
   baseURL: backEndUrl,
